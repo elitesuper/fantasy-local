f01baa64049af9c9d59d20a38be48d30d33fada8
lpissinali <pissinali@gmail.com>
2023-02-13 14:51:40 +0100

    Adding friends

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 
 import styles from './friends.module';
+import {Magnifier} from "../../images/Magnifier";
 
 const Friends = () => {
     return (
-        <div>
-            Friends
-        </div>
+        <>
+            <div className={styles.search}>
+                <div className={styles.text}>
+                    <span className={styles.name}>Friends</span>
+                    <a href="#">Invite friends</a>
+                </div>
+                <div className={styles.field}>
+                    <button><Magnifier /></button>
+                    <input
+                        type='text'
+                        name='search'
+                        id='search'
+                        placeholder='Search'
+                    />
+                </div>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.avatar}>
+                    <img src="/images/user.png" alt=""/>
+                </div>
+                <div className={styles.user}>
+                    <div className={styles.name}>Maja Svensk</div>
+                    <div>+45 28 55 88 74</div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,12 +1,60 @@
 import React from "react";
 
 import styles from './leaderboard.module';
+import classNames from "classnames";
 
 const Leaderboard = () => {
     return (
-        <div>
-            Leaderboard
-        </div>
+        <>
+            <div className="box">
+                <div className="boxTitle bn">Leaderboard</div>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.avatar}>
+                    <img src="/images/user.png" alt=""/>
+                </div>
+                <div className={styles.user}>
+                    <div className={styles.name}>Maja Svensk</div>
+                    <div>
+                        <span>01</span> | Win: 9 | Lost: 1
+                    </div>
+                </div>
+                <div className={styles.score}>
+                    Season Points
+                    <div className={styles.points}>2305.0</div>
+                </div>
+            </div>
+            <div className={classNames(styles.item, styles.selected)}>
+                <div className={styles.avatar}>
+                    <img src="/images/user.png" alt=""/>
+                </div>
+                <div className={styles.user}>
+                    <div className={styles.name}>You</div>
+                    <div>
+                        <span>02</span> | Win: 9 | Lost: 1
+                    </div>
+                </div>
+                <div className={styles.score}>
+                    Season Points
+                    <div className={styles.points}>2305.0</div>
+                </div>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.avatar}>
+                    <img src="/images/user.png" alt=""/>
+                </div>
+                <div className={styles.user}>
+                    <div className={styles.name}>Thomas Andersen</div>
+                    <div>
+                        <span>03</span> | Win: 9 | Lost: 1
+                    </div>
+                </div>
+                <div className={styles.score}>
+                    Season Points
+                    <div className={styles.points}>2305.0</div>
+                </div>
+            </div>
+        </>
     );
 };
 
