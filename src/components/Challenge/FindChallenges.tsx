0b3a8bf3651ef35420f9caf98f412d3cce7949d5
lpissinali <pissinali@gmail.com>
2023-03-07 14:26:55 +0100

    Adding tabs

--- src/components/Challenge/FindChallenges.tsx
+++ src/components/Challenge/FindChallenges.tsx
@@ -1,32 +1,36 @@
 import React from "react";
 
-import classNames from "classnames";
 import {Cricket} from "../../images/Cricket";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
 import styles from './challenges.module';
 
 const FindChallenges = () => {
     return (
-        <>
+        <Tabs selectedTabClassName="selected">
             <div className="box">
                 <div className="boxTitle">Find Challenges</div>
-                <div className="boxContent">
-                    <div className={styles.tabs}>
-                        <span className={classNames(styles.tab, styles.selected)}>Privat</span>
-                        <span className={styles.tab}>Open</span>
+                <TabList className="tabs">
+                    <Tab className="tab">Privat</Tab>
+                    <Tab className="tab">Open</Tab>
+                </TabList>
+            </div>
+            <TabPanel>
+                <div className={styles.challenge}>
+                    <div className={styles.ball}>
+                        <Cricket/>
+                    </div>
+                    <div className={styles.info}>
+                        <strong>India vs. Australia in India Test Series</strong>
+                        <div>Participants: Rene, Josip, Sandra, Thomas, …</div>
+                        <small>Thursday at 05:00 - 06:00 I Create by: You</small>
                     </div>
                 </div>
-            </div>
-            <div className={styles.challenge}>
-                <div className={styles.ball}>
-                    <Cricket/>
-                </div>
-                <div className={styles.info}>
-                    <strong>India vs. Australia in India Test Series</strong>
-                    <div>Participants: Rene, Josip, Sandra, Thomas, …</div>
-                    <small>Thursday at 05:00 - 06:00 I Create by: You</small>
-                </div>
-            </div>
-        </>
+            </TabPanel>
+            <TabPanel>
+                <div className={styles.noChallenges}>There's no challenges available</div>
+            </TabPanel>
+        </Tabs>
     );
 };
 
