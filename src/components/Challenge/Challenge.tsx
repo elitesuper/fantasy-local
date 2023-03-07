37d3f4cabe51fe0c7cc6b57e869ee5c74a34edc0
lpissinali <pissinali@gmail.com>
2023-03-07 17:52:31 +0100

    Adding challenges page

--- src/components/Challenge/Challenge.tsx
+++ src/components/Challenge/Challenge.tsx
@@ -1,8 +1,40 @@
 import React from "react";
 
+import {Cricket} from "../../images/Cricket";
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+
+import styles from './challenges.module';
+
 const Challenge = () => {
     return (
-        <div>challenge page</div>
+        <Tabs selectedTabClassName="selected">
+            <div className="box">
+                <div className="boxTitle">Challenges</div>
+                <TabList className="tabs">
+                    <Tab className="tab">Open</Tab>
+                    <Tab className="tab">Started</Tab>
+                    <Tab className="tab">Finished</Tab>
+                </TabList>
+            </div>
+            <TabPanel>
+                <div className={styles.challenge}>
+                    <div className={styles.ball}>
+                        <Cricket/>
+                    </div>
+                    <div className={styles.info}>
+                        <strong>India vs. Australia in India Test Series</strong>
+                        <div>Participants: Rene, Josip, Sandra, Thomas, …</div>
+                        <small>Thursday at 05:00 - 06:00 I Create by: You</small>
+                    </div>
+                </div>
+            </TabPanel>
+            <TabPanel>
+                <div className={styles.noChallenges}>There's no challenges available</div>
+            </TabPanel>
+            <TabPanel>
+                <div className={styles.noChallenges}>There's no challenges available</div>
+            </TabPanel>
+        </Tabs>
     );
 };
 
