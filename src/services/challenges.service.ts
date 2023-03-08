6c22c3680860a221bce6a9404b71c6f98b040533
fullstackdev <yaolei.guru@gmail.com>
2023-03-08 01:40:24 -0800

    change api url

--- src/services/challenges.service.ts
+++ src/services/challenges.service.ts
@@ -1,17 +1,19 @@
 import {authHeader} from "./auth-header";
 import axios from "axios";
 
+const baseUrl  = process.env.PROXY ?? process.env.COMMON_BASE_URL;
+
 export class ChallengesService {
     
     static readonly shared: ChallengesService = new ChallengesService();
 
     async findChallenge(reqBody: { userId: String; challengePrivacy: Boolean; pageIndex: Number, pageSize: Number }) {
         const headers = await authHeader();
-        return axios.post('/common-api/api/find-challenges', reqBody, { headers: headers });
+        return axios.post(baseUrl + '/api/find-challenges', reqBody, { headers: headers });
     }
 
     async matchesDataNoPlayers() {
         const headers = await authHeader();
-        return axios.post('/common-api/api/matches-data-no-players', {}, { headers: headers });
+        return axios.post(baseUrl + '/api/matches-data-no-players', {}, { headers: headers });
     }
 }
--- src/services/friends.service.ts
+++ src/services/friends.service.ts
@@ -1,6 +1,8 @@
 import {authHeader} from "./auth-header";
 import axios from "axios";
 
+const baseUrl  = process.env.PROXY ?? process.env.COMMON_BASE_URL;
+
 export class FriendsService {
     
     static readonly shared: FriendsService = new FriendsService();
@@ -8,6 +10,6 @@ export class FriendsService {
 
     async getFriends(filter: { userId: String; searchKey: String }) {
         const headers = await authHeader();
-        return axios.post('/common-api/api/users/my-friends', filter, { headers: headers });
+        return axios.post(baseUrl + '/api/users/my-friends', filter, { headers: headers });
     }
 }
