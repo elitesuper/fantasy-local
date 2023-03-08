c9e33ee65329d6cd8c2fb647b57e2eb576f8645b
lpissinali <pissinali@gmail.com>
2023-03-08 08:42:27 +0100

    Adding static pages

--- src/components/StaticPages/About.tsx
+++ src/components/StaticPages/About.tsx
@@ -4,8 +4,12 @@ const About = () => {
     return (
         <>
             <div className="box">
-                <div className="boxTitle">About Betway</div>
-                <div className="boxContent">Lorem ipsum dolor sit amet, consectetur adcing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim niam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est esse cillum dolor laborum.</div>
+                <div className="boxTitle">All About Us</div>
+                <div className="boxContent">
+                    <p>CRICKET FANTASY makes it possible to challenge and be challenged by friends. Create your own private challenges for all types of cricket matches being held across the world. Make your own prizes or choose from list of skates we created like <strong>"Winner will pay the dinner for all"</strong></p><br/>
+                    <p>CRICKET FANTASY is a social fantasy platform for cricket where you can create challenges, invite friends to challenges, win prizes like tickets, jerseys, and other prizes. We have a weekly open challenge and its free to join for anyone. </p><br/>
+                    <p>Select the formation either 4, 8, 11 players and choose players from you favorite teams. You can choose players from either team or you can select auto-fill option to let us choose best performance players for you.</p>
+                </div>
             </div>
         </>
     );
